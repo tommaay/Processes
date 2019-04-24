803da647912b81be963172f1c764dfe973be0510
ToMMaaY <thuynh629@gmail.com>
2019-04-24 14:23:35 -0700

    completed ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,26 @@
 int main(void)
 {
     // Your code here
+    int x = 3;
+    int pid = fork();
+
+    printf("x is current %d\n", x); // Print 3
+
+    if (pid < 0)
+    {
+        perror("fork failed\n");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        x = 5;
+        printf("I am a child and x is currently: %d\n", x);
+    }
+    else
+    {
+        int ppid = waitpid(pid, NULL, 0);
+        printf("I am a parent and x is currently: %d\n", x);
+    }
 
     return 0;
 }
